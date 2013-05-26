0f056754baf13a932c16efccd9be866b43ca3774
mglagola <mark.glagola@gmail.com>
2013-05-26 01:03:33 -0500

    update before cocoapods

--- KrayFilters/Classes/Controllers/KFCameraViewController.m
+++ KrayFilters/Classes/Controllers/KFCameraViewController.m
@@ -8,9 +8,11 @@
 
 #import "KFCameraViewController.h"
 #import "GPUImage.h"
+#import "MGInstagram.h"
 
 @interface KFCameraViewController () {
     GPUImageVideoCamera *camera;
+    GPUImageFilter *sketchFilter;
 }
 
 @end
@@ -29,7 +31,7 @@ NSUInteger UIViewAutoresizingMaskAll() {
 
     camera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1920x1080 cameraPosition:AVCaptureDevicePositionBack];
     camera.outputImageOrientation = self.interfaceOrientation;
-    GPUImageFilter *sketchFilter = [[GPUImageSketchFilter alloc] init];
+    sketchFilter = [[GPUImageSketchFilter alloc] init];
     [camera addTarget:sketchFilter];
 
     GPUImageView *imageView = [[GPUImageView alloc] initWithFrame:self.view.frame];
@@ -37,7 +39,21 @@ NSUInteger UIViewAutoresizingMaskAll() {
     [self.view addSubview:imageView];
     [sketchFilter addTarget:imageView];
     
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage *bImage = [UIImage imageNamed:@"camera-button"];
+    CGSize bSize = CGSizeMake(60, 46);
+    button.frame = CGRectMake(self.view.width*.5f-bSize.width*.5f, self.view.height-bSize.height*1.1f, bSize.width, bSize.height);
+    [button setImage:bImage forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(captureButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
     [camera startCameraCapture];
+    
+}
+
+- (void) captureButtonPressed {
+    UIImage *image = [sketchFilter imageFromCurrentlyProcessedOutput];
+    UIImageWriteToSavedPhotosAlbum(image, NULL, NULL, NULL);
 }
 
 - (void) willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration  {
